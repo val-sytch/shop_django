a24bcf9f4d3a0a63632e0d19724d7164cff00f0c
Serhiy <mcseruy@gmail.com>
2016-10-05 21:46:57 +0300

    update config.files

--- djog/config/config.py
+++ djog/config/config.py
@@ -5,20 +5,20 @@ path = os.path.realpath(os.path.dirname(__file__))
 config = configparser.ConfigParser()
 config.read(os.path.join(path, 'config.ini'))
 
-DATABASE_NAME = config.get('DATABASE', 'DATABASE_NAME')
-DATABASE_ENGINE = config.get('DATABASE', 'DATABASE_ENGINE')
-DATABASE_USER = config.get('DATABASE', 'DATABASE_USER')
-DATABASE_PASSWORD = config.get('DATABASE', 'DATABASE_PASSWORD')
+DATABASE_NAME = config.get('DATABASE', 'database_name')
+DATABASE_ENGINE = config.get('DATABASE', 'database_engine')
+DATABASE_USER = config.get('DATABASE', 'database_username')
+DATABASE_PASSWORD = config.get('DATABASE', 'database_password')
 
-DEBUG = config.get('OTHER', 'DEBUG')
+DEBUG = config.get('OTHER', 'debug')
 
-SECRET_KEY = config.get('SECRET', 'SECRET_KEY')
+SECRET_KEY = config.get('SECRET', 'secret_key')
 
 APP_DIR = path.rsplit('/djog', 1)[0]
-STATIC_URL = config.get('PATH', 'STATIC_URL')
-UPLOAD_IMAGE = os.path.join(APP_DIR, config.get('PATH', 'UPLOAD_IMAGE'))
-WATERMARK = os.path.join(APP_DIR, config.get('PATH', 'WATERMARK'))
+STATIC_URL = config.get('PATH', 'static_url')
+UPLOAD_IMAGE = os.path.join(APP_DIR, config.get('PATH', 'upload_image'))
+WATERMARK = os.path.join(APP_DIR, config.get('PATH', 'watermark'))
 
-IMG_WIDTH_REQUIR = int(config.get('IMAGERESIZER', 'WIDTH'))
-IMG_HEIGHT_REQUIR = int(config.get('IMAGERESIZER', 'HEIGHT'))
-WATERMARK_OPACITY = float(config.get('IMAGERESIZER', 'WATERMARK_OPACITY'))
+IMG_WIDTH_REQUIR = int(config.get('IMAGERESIZER', 'width'))
+IMG_HEIGHT_REQUIR = int(config.get('IMAGERESIZER', 'height'))
+WATERMARK_OPACITY = float(config.get('IMAGERESIZER', 'watermark_opacity'))
