d09467f5b7febcf433f94a5207113574aec67c21
Chubasic <joebereza@gmail.com>
2016-10-14 20:38:39 +0300

    Typos:(

--- djog/admin.py
+++ djog/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from djog.models.dogs import Breeds, Dogs
+from djog.models.models import Breeds, Dogs
 from djog.user.users import SingUp
 # Register your models here.
