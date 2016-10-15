ae104c936740902248c6544a8a11784d3a23cb5e
Oleksandr Petrov <optrv@users.noreply.github.com>
2016-10-15 15:41:03 +0300

    model dogs corrected

--- djog/models/model_dogs.py
+++ djog/models/model_dogs.py
@@ -13,23 +13,22 @@ class Breeds(models.Model):
         verbose_name_plural = "Breeds"
 
     def __str__(self):
-        return 'Breed %s' % self.breed
+        return self.breed
 
 
 class Dogs(models.Model):
     """
     Main class
     """
+    alias = models.SlugField(verbose_name='Item alias')
     breeds = models.ForeignKey(Breeds)
     description = models.TextField(max_length=1000, verbose_name="Short description")
     image = models.CharField(max_length=100, verbose_name='Image URL')
     price = models.IntegerField(default=0, verbose_name='Price')
     age = models.IntegerField(default=0, verbose_name="Age")
     quantity = models.IntegerField(default=0, verbose_name='Quantity')
-    alias = models.SlugField(verbose_name='Item alias')
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
-
     def __str__(self):
-        return 'Breed %s' % self.breeds
+        return self.alias
