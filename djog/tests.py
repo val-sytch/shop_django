4329e0196ccd4d6315480c45321c86b6e2ca6a1b
Chubasic <joebereza@gmail.com>
2016-10-15 20:20:30 +0300

    Simple user model tests

--- djog/tests.py
+++ djog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.utils import timezone
+"""from djog.models.model_dogs import Breeds, Dogs Not ready yet
+"""
+from djog.models.model_user_registration import SingUp
 
 # Create your tests here.
+
+
+class SingUpModelTest(TestCase):
+
+    def craeate_SingUp(self, username="Username", email="test@example.com", first_name="Name", second_name="Sidorov",
+                       password="testpass"):
+        return SingUp.objects.craeate(username=username, email=email, first_name=first_name, second_name=second_name,
+                                      password=password, regis_date=timezone.now())
+
+    def test_username_creation(self):
+        first_name = SingUp(first_name="Somedude")
+        self.assertEqual(str(first_name), first_name.first_name)
