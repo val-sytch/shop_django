3c859185f106f9142b3a0b20995a515ac45ebde7
val-sytch <val.sytch@gmail.com>
2016-10-16 21:39:00 +0300

    fabfile.py updated:add func for launching parser and img downloader

--- fabfile.py
+++ fabfile.py
@@ -38,3 +38,17 @@ def runtest():
     Run tests in djog
     """
     local('python manage.py test djog')
+
+
+def write_breeds_in_db():
+    """
+    Launch script, which parse wiki and write all breeds to database
+    """
+    local('python djog/servises/parser_dog_breeds.py')
+
+
+def download_img():
+    """
+    Launch script, which download pictures for each breed in db
+    """
+    local('python djog/servises/google_img_downloader.py')
