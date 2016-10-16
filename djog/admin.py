c9524b0178d60d6600fbde9934a3ffc16ec81ed5
Edisontom <romazinchuk@gmail.com>
2016-10-16 14:57:50 +0300

    Update admin.py

--- djog/admin.py
+++ djog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from djog.models.models import Breeds, Dogs
+from djog.user.models import SingUp
 
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('breeds', 'price')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'breeds')
+
+
+admin.site.register(Dogs, ItemAdmin)
+admin.site.register(Breeds, CategoryAdmin)
